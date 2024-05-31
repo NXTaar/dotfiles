164ef1ff1586a345958eea3459c905e33474d1d7
Valeriy Sagatovskiy <code@vsgt.dev>
2024-05-31 03:14:26 +0300

    fixed treesitter init

--- nvim/lua/nxtaar/editor/treesitter.lua
+++ nvim/lua/nxtaar/editor/treesitter.lua
@@ -1,17 +1,18 @@
 local M = {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    main = 'nvim-treesitter.configs',
     opts = {
         -- A list of parser names, or "all" (the five listed parsers should always be installed)
         ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query' },
-    
+
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
-    
+
         -- Automatically install missing parsers when entering buffer
         -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
         auto_install = true,
-    
+
         highlight = {
             enable = true,
             -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
