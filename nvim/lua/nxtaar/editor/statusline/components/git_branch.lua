ed05661567290ecfea13b0d06c0623f22af9a933
Сагатовский Валерий <sagatovskii@tochka.com>
2026-01-29 13:41:48 +0300

    nvim: adjusted colors and the filename of the statuline

--- nvim/lua/nxtaar/editor/statusline/components/git_branch.lua
+++ nvim/lua/nxtaar/editor/statusline/components/git_branch.lua
@@ -1,13 +1,17 @@
 local conditions = require('heirline.conditions')
-local utils = require('heirline.utils')
 local Pill = require('nxtaar.editor.statusline.components.ui.pill')
+local Color = require('nxtaar.utils.color')
+
+local base_color = Color.from_hl_group('WarningMsg', 'fg')
+local base_color_rgb = base_color:to_rgb()
+
+local icon_bg = base_color_rgb
+local icon_color = '#000000'
+local content_fg = base_color_rgb
+local content_bg = base_color:shade(0.75):to_rgb()
 
 return {
     condition = conditions.is_git_repo,
-
-    static = {
-        git_hl = utils.get_highlight('DiagnosticVirtualTextWarn'),
-    },
 
     init = function(self)
         self.status_dict = vim.b.gitsigns_status_dict
@@ -18,19 +22,15 @@ return {
 
     Pill({
         icon = '',
-        icon_color = '#000000',
-        pill_color = function(self)
-            return self.git_hl.fg
-        end,
+        icon_color = icon_color,
+        pill_color = icon_bg,
         content = function(self)
             return self.status_dict.head
         end,
-        content_hl = function(self)
-            return {
-                fg = self.git_hl.fg,
-                bg = self.git_hl.bg,
-                bold = true,
-            }
-        end,
+        content_hl = {
+            fg = content_fg,
+            bg = content_bg,
+            bold = true,
+        },
     }),
 }
