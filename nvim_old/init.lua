2ed5d39eeb9769595865132060dafa69187ca6f8
Сагатовский Валерий <sagatovskii@tochka.com>
2026-05-26 15:16:45 +0300

    old config backup

---
+++ nvim_old/init.lua
@@ -0,0 +1,10 @@
+require('nxtaar.spec')
+require('nxtaar.settings')
+require('nxtaar.commands')
+require('nxtaar.keymapper')
+
+spec('nxtaar.lsp.setup')
+spec('nxtaar.editor')
+spec('nxtaar.tools')
+
+require('nxtaar.launch')
