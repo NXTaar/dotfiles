978293bc7df0833a87bf4e2aca8d4e3a70489d84
Сагатовский Валерий <sagatovskii@tochka.com>
2026-05-26 15:16:45 +0300

    nvim: added colorscheme

--- nvim/lua/nxtaar/system/plugin_manager.lua
+++ nvim/lua/nxtaar/system/plugin_manager.lua
@@ -14,11 +14,16 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
+local specs = {
+    "nxtaar.plugins",
+    "nxtaar.editor"
+}
+
 -- Setup lazy.nvim
 require("lazy").setup({
-    spec = {{
-        import = "nxtaar.plugins"
-    }},
+    spec = vim.tbl_map(function (item)
+        return {import = item}
+    end,specs),
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
     install = {
